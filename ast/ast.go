e0296e59c21e2094afe59302e901af797c741816
Shub562 <shubhampandey820@gmail.com>
2025-02-02 18:05:55 -0600

    parsing let statements

---
+++ ast/ast.go
@@ -0,0 +1,53 @@
+package ast
+
+import "writing-an-interpreter-in-go/token"
+
+type Node interface {
+	// Returns the literal value of the token it is associated with
+	TokenLiteral() string
+}
+
+type Statement interface {
+	Node
+	statementNode()
+}
+
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+// Root node of AST.
+// Every valid Monkey program is a series of statements.
+// These statements are contained in the Program.Statements,
+// which is just a slice of AST nodes that implement the Statement interface.
+type Program struct {
+	Statements []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+type LetStatement struct {
+	Token token.Token
+	Name  *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode() {}
+
+func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+
+type Identifier struct {
+	Token token.Token
+	Value string
+}
+
+func (i *Identifier) expressionNode() {}
+
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
